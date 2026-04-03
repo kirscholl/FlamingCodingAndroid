45224ef2d2386dbcf02d4708257d3cec6ad0f123
FlaMing <292712886@qq.com>
2026-04-03 16:32:49 +0800

    feature：算法目录重构，报错修复

--- app/src/main/java/com/example/flamingcoding/Algorithm/TreeAlgorithm.kt
+++ app/src/main/java/com/example/flamingcoding/Algorithm/TreeAlgorithm.kt
@@ -2,15 +2,5 @@ package com.example.flamingcoding.Algorithm
 
 // 各种树的算法题
 class TreeAlgorithm {
-    class TreeNode(var `val`: Int) {
-        var left: TreeNode? = null
-        var right: TreeNode? = null
-    }
-
-    fun levelOrder(root: TreeNode?): List<List<Int>> {
-
-        val queue = ArrayDeque<String>()
-
-    }
 
 }
